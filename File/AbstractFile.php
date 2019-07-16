fc5b98202abdc7b1a15a7b8db26c1d8d5d3b1add
knesmeyanov <k.nesmeyanov@rambler-co.ru>
2019-07-16 21:13:05 +0300

    Typo

--- File/AbstractFile.php
+++ File/AbstractFile.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of Phplrt package.
+ * This file is part of phplrt package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -9,9 +9,9 @@ declare(strict_types=1);
 
 namespace Phplrt\Io\File;
 
-use Phplrt\Io\Readable;
+use Phplrt\Contracts\Io\Readable;
+use Phplrt\Contracts\Position\PositionInterface;
 use Phplrt\Position\Position;
-use Phplrt\Position\PositionInterface;
 
 /**
  * Class AbstractFile
